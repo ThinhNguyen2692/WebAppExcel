a608a304e7c575bc97fdf57e35aaa96cc0ca9a03
nguyenthinh28902 <nndthinh28902@gmail.com>
2024-03-27 14:12:33 +0700

    Remove ignored files

---
+++ WebAppExportExcel/Models/FileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppExportExcel.Models
+{
+    public class FileViewModel
+    {
+        
+        public FileViewModel() { }
+
+        [DisplayName("Chọn file Ticktay")]
+        [Required]
+        public IFormFile filetick { get; set; }
+        [Required]
+        [DisplayName("Chọn file công")]
+        public IFormFile filecong { get; set; }
+    }
+}
